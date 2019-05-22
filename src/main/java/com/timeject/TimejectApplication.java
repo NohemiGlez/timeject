647cf462a9a6e74df75897403c39c542fda4853e
NohemiGlez <ngonzalez.swe@gmail.com>
2019-05-21 22:48:40 -0600

    Add heroku postgres database config and connection

--- src/main/java/com/timeject/TimejectApplication.java
+++ src/main/java/com/timeject/TimejectApplication.java
@@ -2,6 +2,7 @@ package com.timeject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class TimejectApplication {
@@ -10,4 +11,7 @@ public class TimejectApplication {
 		SpringApplication.run(TimejectApplication.class, args);
 	}
 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TimejectApplication.class);
+	}
 }
