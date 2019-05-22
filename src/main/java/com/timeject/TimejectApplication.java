1071e1c5158c6197eec82cc4ea700c23eb26997a
NohemiGlez <ngonzalez.swe@gmail.com>
2019-05-21 20:45:52 -0600

    Create a spring boot app

---
+++ src/main/java/com/timeject/TimejectApplication.java
@@ -0,0 +1,13 @@
+package com.timeject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimejectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimejectApplication.class, args);
+	}
+
+}
