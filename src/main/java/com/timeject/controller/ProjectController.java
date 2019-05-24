ac0782db85590d9fc1c3e52abd1caf85a8ef485b
omaru <omarux@gmail.com>
2019-05-23 19:25:27 -0600

    internal server error exception

--- src/main/java/com/timeject/controller/ProjectController.java
+++ src/main/java/com/timeject/controller/ProjectController.java
@@ -2,10 +2,14 @@ package com.timeject.controller;
 
 import com.timeject.model.Project;
 import com.timeject.repository.ProjectRepository;
+import javassist.NotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpServerErrorException;
 
 import javax.inject.Inject;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/projects")
@@ -26,7 +30,10 @@ public class ProjectController {
 	@PostMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Project addProject(@RequestBody Project project) {
 		projectRepository.save(project);
-		return projectRepository.findById(project.getId());
+		Optional<Project> projectOptional = projectRepository.findById(project.getId());
+		if (!projectOptional.isPresent())
+			throw new HttpServerErrorException(HttpStatus.INTERNAL_SERVER_ERROR,"unable to store project");
+		return projectOptional.get();
 	}
 	
 }
